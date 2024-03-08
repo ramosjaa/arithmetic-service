b6de7cc5bbd2877cdef540e1d67a3b662766837f
Jack Ramos <ramos.jack525@yahoo.com>
2024-03-08 00:39:15 -0500

    addded more operation functions

--- src/arithmetica.js
+++ src/arithmetica.js
@@ -2,4 +2,16 @@ function add(n, m){
     return n + m;
 }
 
+function subtract(n, m){
+    return n - m;
+}
+
+function multiply(n, m){
+    return n * m;
+}
+
+function divide(n, m){
+    return n / m;
+}
+
 module.exports = { add };
